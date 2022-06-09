b35962f97a547c075db48497b14c64267a2b7141
AhmedArshed <p176099@nu.edu.pk>
2022-06-09 18:18:46 +0500

    update

--- test.py
+++ test.py
@@ -1,28 +1,22 @@
+import re
 import requests
 from bs4 import BeautifulSoup
-import random
-import scrapy
-import requests
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
-import sys
+from utils.helper import *
+import pymongo
+from pymongo import MongoClient, InsertOne
+import os
 
-def get_url():
-    url = 'https://www.southlandelectrical.com/sitemap.xml'
-    software_names = [SoftwareName.CHROME.value]
-    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
-    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
-    user_agent = user_agent_rotator.get_random_user_agent()
-    headers =  {
-        'Accept': '*/*',
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'User-Agent': user_agent,
-        'X-Requested-With': 'XMLHttpRequest'
-    }
-    page = requests.get(url,headers=headers)
-    print(page)
+def controlparts_url():
+    outputs = []
+    url = 'https://controlparts.com/sitemap_products_1.xml?from=4497408295009&to=4763190591585'
+    page = requests.get(url)
     sitemap_index = BeautifulSoup(page.content, 'html.parser')
     sitemap_url = [element.text for element in sitemap_index.findAll('loc')]
-    return sitemap_url
-
-print(get_url())
+    for data in sitemap_url:
+        result = {
+            "competitor": "controlparts",
+            "url": data,
+            "scraper_type": "sitemap"
+        }
+        outputs.append(result)
+    print(outputs)
