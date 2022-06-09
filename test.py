dc4624097439edede0e28dce465427a7be966d31
AhmedArshed <p176099@nu.edu.pk>
2022-06-09 17:46:15 +0500

    update test file

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
-lines = list()
+import requests
+from bs4 import BeautifulSoup
+import random
+import scrapy
+import requests
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+import sys
 
-import csv
+def get_url():
+    url = 'https://www.southlandelectrical.com/sitemap.xml'
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+    user_agent = user_agent_rotator.get_random_user_agent()
+    headers =  {
+        'Accept': '*/*',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'User-Agent': user_agent,
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    page = requests.get(url,headers=headers)
+    print(page)
+    sitemap_index = BeautifulSoup(page.content, 'html.parser')
+    sitemap_url = [element.text for element in sitemap_index.findAll('loc')]
+    return sitemap_url
 
-with open('parsed-output.csv', 'r') as readFile:
-
-    reader = csv.reader(readFile)
-
-    for row in reader:
-        lines.append(row)
-        for field in row:
-            if field == "buymybreaker" or field == "southlandelectrical" or field == "chartercontact":
-                lines.remove(row)
-
-with open('test.csv', 'w') as writeFile:
-
-    writer = csv.writer(writeFile)
-
-    writer.writerows(lines)
+print(get_url())
